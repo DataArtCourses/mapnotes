c791cd208a88997e9ddd2fd4cb2307b7b06beaf8
Tehtehteh <tehfvpr@gmail.com>
2017-10-19 23:50:28 +0300

    Added Element to Vue.js.

--- public/src/components/Registration.vue
+++ public/src/components/Registration.vue
@@ -1,30 +1,41 @@
 <template>
  <div>
     <h1>Qwa!</h1>
-    <form id="registarion" @submit.prevent="validateBeforeSubmit">
-        <div>
-          <input name="text" type="text" placeholder="e-mail" v-model="email"><br>
+   <el-row :gutter="20">
+    <el-col :span="12" :offset="6">
+      <el-form :label-position="labelPosition" label-width="100px" :model="formLabelAlign">
+        <el-form-item label="Email">
           <p v-show="wrongEmail && attemptSubmit">Wrong email</p>
-          <input name="password" type="password" placeholder="password" v-model="password"><br>
+          <el-input name="text" type="text" placeholder="e-mail" v-model="formLabelAlign"></el-input>
+        </el-form-item>
+        <el-form-item label="Password">
           <p v-show="wrongPasswordLength && attemptSubmit">Password length from 8 to 16</p>
           <p v-show="wrongPassword && attemptSubmit">In the password must be present digit and letter</p>
-          <input name="password_confirmation" type="password" placeholder="password, again" v-model="password_confirmation">
-          <p v-show="wrongPasswordConfirmation && attemptSubmit">Wrong password confirmation</p>
-        </div>
-      <button type="submit">Send</button>
-  </form>
+          <el-input name="password" type="password" placeholder="password" v-model="password"></el-input>
+        </el-form-item>
+        <el-form-item label="Password">
+          <el-input name="password_confirmation" type="password" placeholder="repeat password" v-model="password_confirmation"></el-input>
+            <p v-show="wrongPasswordConfirmation && attemptSubmit">Wrong password confirmation</p>
+        </el-form-item>
+        <el-button type="submit">Send</el-button>
+      </el-form>
+    </el-col>
+  </el-row>
+
   <router-link to="/login">Sign In</router-link>
-        
+
  </div>
 </template>
 <script>
 import axios from 'axios'
+import ElButton from '../../node_modules/element-ui/packages/button/src/button.vue'
 
 const API_BASE = 'http://127.0.0.1:8000/api/'
 const reEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
 const rePassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/
 
 export default {
+  components: {ElButton},
   data () {
     return {
       email: '',
@@ -50,7 +61,7 @@ export default {
   methods: {
     validateBeforeSubmit () {
       this.attemptSubmit = true
-      if (!this.wrongEmailv && !this.wrongPasswordLength && !this.wrongPassword && !this.wrongPasswordConfirmation) {
+      if (!this.wrongEmail && !this.wrongPasswordLength && !this.wrongPassword && !this.wrongPasswordConfirmation) {
         axios.post(API_BASE + 'register', {
           email: this.email,
           password: this.password
@@ -67,34 +78,6 @@ export default {
         })
       }
     }
-    // validateBeforeSubmit () {
-    //   this.$validator
-    //     .validateAll()
-    //     .then(response => {
-    //       // Validation success if response === true
-    //       if (this.email && this.password) {
-    //         axios.post(API_BASE, {
-    //           email: this.email,
-    //           password: this.password
-    //         })
-    //         .then(response => {
-    //           console.log(response)
-    //           if (response.status === 200) {
-    //             this.$router.push({path: '/login'})
-    //           }
-    //         })
-    //         .catch(e => {
-    //           console.error(e)
-    //         })
-    //         console.log({'email': this.email, 'password': this.password})
-    //         // this.$router.push({path: '/login'})
-    //       }
-    //     })
-    //     .catch(e => {
-    //       // Catch errors
-    //       console.error(e)
-    //     })
-    // }
   }
 }
 </script>
